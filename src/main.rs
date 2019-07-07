dd702f275a0023979c4ff0b8d1642f6f5ca81a3c
Hadrien <hadrien@psydk.org>
2019-07-07 17:50:53 +0200

    Use header bar

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 extern crate gtk;
+extern crate gio;
+
 use gtk::prelude::*;
-use gtk::{Window, WindowType, Entry, Label, Box, Orientation, Menu, MenuBar, MenuItem,
-	AboutDialog, License, CssProvider, StyleContext};
+use gio::prelude::*;
+
+use gio::MenuExt;
+use gio::SimpleAction;
+//use gio::ActionMapExt;
+
+use gtk::{ApplicationWindow};
+
+/*
+//use gio::*;*/
+
+//use glib::{self, Variant};
+use std::env::args;
 
 mod text_canvas;
 mod eval;
@@ -11,6 +24,7 @@ mod funcs;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
+#[derive(Clone)]
 struct AppArgs {
 	debug_mode: bool,
 	expression: String
@@ -52,6 +66,111 @@ fn test_parse_app_args() {
 	assert_eq!(app_args3.expression, "42");
 }
 
+struct Header {
+	pub header_bar: gtk::HeaderBar,
+	pub hamburger_button: gtk::MenuButton
+}
+
+impl Header {
+	fn new() -> Header {
+		let header_bar = gtk::HeaderBar::new();
+		header_bar.set_title("Dkalc");
+		header_bar.set_show_close_button(true);
+
+		let hamburger_button = gtk::MenuButton::new();
+		let hamburger_image = gtk::Image::new_from_icon_name("open-menu-symbolic", gtk::IconSize::Button.into());
+		hamburger_button.set_image(&hamburger_image);
+		header_bar.pack_end(&hamburger_button);
+
+		let menu = gio::Menu::new();
+		//menu.append("Preferences", "win.preferences");
+	  	menu.append("About", "win.about");
+
+		let popover = gtk::Popover::new_from_model(&hamburger_button, &menu);
+		hamburger_button.set_popover(&popover);
+
+		Header { header_bar, hamburger_button }
+	}
+}
+
+/*
+fn add_action<M, A>(map: &M, name: &str, action: A)
+	where M: ActionMapExt,
+		for<'r, 's> A: Fn(&'r gio::SimpleAction, &'s Option<glib::Variant>) + 'static
+{
+	let sa = SimpleAction::new("about", None);
+	map.add_action(&sa);
+	sa.connect_activate(action);
+}*/
+
+fn show_about(window: &ApplicationWindow) {
+	let ad = gtk::AboutDialog::new();
+	ad.set_authors(&["Hadrien Nilsson"]);
+	ad.set_website_label(Some("psydk.org"));
+	ad.set_website(Some("http://psydk.org"));
+	ad.set_title("About dkalc");
+	ad.set_program_name("dkalc");
+	ad.set_version(Some(VERSION));
+	ad.set_license_type(gtk::License::Gpl20);
+	ad.set_transient_for(Some(window));
+	ad.set_logo_icon_name("dkalc");
+	ad.run();
+	ad.destroy();
+}
+
+fn build_ui(app: &gtk::Application, app_args: &AppArgs) {
+	let window = ApplicationWindow::new(app);
+
+	let header = Header::new();
+	window.set_titlebar(&header.header_bar);
+	window.set_wmclass("dkalc", "Dkalc");
+	window.set_default_size(350, 100);
+
+	////////////////////////////////////////////////////////////////
+	let gtk_box = gtk::Box::new(gtk::Orientation::Vertical, 3);
+	window.add(&gtk_box);
+
+	let label = gtk::Label::new(Some("0"));
+	gtk::WidgetExt::set_name(&label, "result");
+	gtk_box.pack_start(&label, true, true, 0);
+
+	let entry = gtk::Entry::new();
+	gtk_box.pack_start(&entry, true, true, 0);
+
+	let css_provider = gtk::CssProvider::new();
+	let css = "#result { font-family: monospace; font-size: 15px; }";
+	if let Err(err) = css_provider.load_from_data(css.as_bytes()) {
+		println!("css_provider.load_from_data failed: {}", err);
+		return;
+	}
+	gtk::StyleContext::add_provider_for_screen(
+		&gtk::WidgetExt::get_screen(&window).unwrap(),
+		&css_provider,
+		800 // gtk_sys::GTK_STYLE_PROVIDER_PRIORITY_USER
+		);
+
+	entry.set_text(&app_args.expression);
+	entry.set_position(-1);
+	let result = eval::eval_input_debug(&app_args.expression, app_args.debug_mode);
+	label.set_label(&result);
+
+	entry.connect_changed({
+		let debug_mode = app_args.debug_mode;
+		move |arg| {
+			if let Some(str) = arg.get_chars(0, -1) {
+				let result = eval::eval_input_debug(&str, debug_mode);
+				label.set_label(&result);
+			}
+		}
+	});
+
+	window.show_all();
+
+	let sa = SimpleAction::new("about", None);
+	window.add_action(&sa);
+	sa.connect_activate(move |_, _| { show_about(&window); });
+}
+
 fn main() {
 	let app_args = parse_app_args(std::env::args().collect());
 
@@ -69,96 +188,11 @@ fn main() {
 		).to_string()
 	);*/
 
+	let app = gtk::Application::new("psydk.dkalc",
+		gio::ApplicationFlags::FLAGS_NONE)
+			.expect("Application::new failed");
+	app.connect_startup(move |arg| build_ui(arg, &app_args));
 
-	if gtk::init().is_err() {
-		println!("Failed to initialize GTK.");
-		return;
-	}
-
-	let window = Window::new(WindowType::Toplevel);
-	window.set_title("dkalc");
-	window.set_wmclass("dkalc", "dkalc");
-	window.set_default_size(350, 100);
-
-	let gtk_box = Box::new(Orientation::Vertical, 3);
-	window.add(&gtk_box);
-
-	let file_menu = Menu::new();
-	/*
-	let settings_menu_item = MenuItem::new_with_label("Settings");
-	file_menu.append(&settings_menu_item);
-	let help_menu_item = MenuItem::new_with_label("Help");
-	file_menu.append(&help_menu_item);
-	*/
-	let about_menu_item = MenuItem::new_with_label("About");
-	file_menu.append(&about_menu_item);
-	let quit_menu_item = MenuItem::new_with_label("Quit");
-	file_menu.append(&quit_menu_item);
-
-	let file_menu_item = MenuItem::new_with_label("File");
-	file_menu_item.set_submenu(Some(&file_menu));
-
-	let menu_bar = MenuBar::new();
-	menu_bar.append(&file_menu_item);
-
-	gtk_box.pack_start(&menu_bar, true, true, 0);
-
-	let label = Label::new(Some("0"));
-	gtk::WidgetExt::set_name(&label, "result");
-	gtk_box.pack_start(&label, true, true, 0);
-
-	let entry = Entry::new();
-	gtk_box.pack_start(&entry, true, true, 0);
-
-
-	let css_provider = CssProvider::new();
-	let css = "#result { font-family: monospace; font-size: 15px; }";
-	if let Err(err) = css_provider.load_from_data(css.as_bytes()) {
-		println!("css_provider.load_from_data failed: {}", err);
-		return;
-	}
-	StyleContext::add_provider_for_screen(
-		&gtk::WidgetExt::get_screen(&window).unwrap(),
-		&css_provider,
-		800 // gtk_sys::GTK_STYLE_PROVIDER_PRIORITY_USER
-		);
-
-	window.connect_delete_event(|_, _| {
-		gtk::main_quit();
-		Inhibit(false)
-	});
-	window.show_all();
-
-	about_menu_item.connect_activate(move |_| {
-		let ad = AboutDialog::new();
-        ad.set_authors(&["Hadrien Nilsson"]);
-        ad.set_website_label(Some("psydk.org"));
-        ad.set_website(Some("http://psydk.org"));
-        ad.set_title("About dkalc");
-		ad.set_program_name("dkalc");
-		ad.set_version(Some(VERSION));
-		ad.set_license_type(License::Gpl20);
-        ad.set_transient_for(Some(&window));
-        ad.run();
-		ad.destroy();
-	});
-
-	quit_menu_item.connect_activate(|_| {
-		gtk::main_quit();
-	});
-
-	entry.set_text(&app_args.expression);
-	entry.set_position(-1);
-	let result = eval::eval_input_debug(&app_args.expression, app_args.debug_mode);
-	label.set_label(&result);
-
-	entry.connect_changed(move |arg| {
-		if let Some(str) = arg.get_chars(0, -1) {
-			let result = eval::eval_input_debug(&str, app_args.debug_mode);
-			label.set_label(&result);
-		}
-	});
-
-	gtk::main();
+	app.connect_activate(|_| {}); // Make GTK happy
+	app.run(&args().collect::<Vec<_>>());
 }
-
